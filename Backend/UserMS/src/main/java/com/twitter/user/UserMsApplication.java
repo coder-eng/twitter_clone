bb913624a80ab42c311af30ffa80696eeadc212b
Priya <ps4695802@gmail.com>
2025-01-04 20:36:25 +0530

    Adding User microservice and Added userDTO

---
+++ Backend/UserMS/src/main/java/com/twitter/user/UserMsApplication.java
@@ -0,0 +1,14 @@
+package com.twitter.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("priya");
+		SpringApplication.run(UserMsApplication.class, args);
+	}
+
+}
---
+++ Backend/UserMS/src/main/java/com/twitter/user/dto/UserDTO.java
@@ -0,0 +1,9 @@
+package com.twitter.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+	private String userName;
+
+}
