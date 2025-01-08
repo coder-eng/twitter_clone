8dc7aae8c309e8050845193d5a6ba815d64ebe1a
Priya <ps4695802@gmail.com>
2025-01-08 21:49:24 +0530

    modified UserDTO

--- Backend/UserMS/src/main/java/com/twitter/user/dto/UserDTO.java
+++ Backend/UserMS/src/main/java/com/twitter/user/dto/UserDTO.java
@@ -1,9 +1,21 @@
 package com.twitter.user.dto;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class UserDTO {
+	private Long id;
 	private String userName;
+	private String email;
+	private String password;
+	private String confirmPassword;
+	private String profileURL;
+	private String coverPhotoURL;
+	private String location;
+	private String website;
+	private String bio;
+	private Date dateOfJoin;
     
 }
