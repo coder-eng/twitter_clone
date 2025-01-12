07a552cbcec7ebfa63289c8aabced47e2c6d74c7
Priya <ps4695802@gmail.com>
2025-01-12 21:03:37 +0530

    added service package

---
+++ Backend/UserMS/src/main/java/com/twitter/user/service/UserService.java
@@ -0,0 +1,8 @@
+package com.twitter.user.service;
+
+import com.twitter.user.dto.UserDTO;
+
+public interface UserService {
+	public Long registerUser(UserDTO userDTO);
+
+}
---
+++ Backend/UserMS/src/main/java/com/twitter/user/service/UserServiceImpl.java
@@ -0,0 +1,5 @@
+package com.twitter.user.service;
+
+public class UserServiceImpl {
+
+}
